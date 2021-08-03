fa5256b7939fd4f53f7c4bcb3ca9ad8c6e7de04b
Demarcus Lester <demarcuslester12@gmail.com>
2021-08-03 08:19:24 -0500

    ex 1 set

---
+++ main.js
@@ -0,0 +1,28 @@
+
+let ex1 = true;
+while (ex1) {
+  // 
+  let response = prompt(
+    ' Enter this sentence "in a galaxy far far away" .'
+  ).toLowerCase(); // user should enter 'y' or 'n'
+  if (response === "in a galaxy far far away" || response === "In A Galaxy Far Far Away") {
+    ex1 = false;
+    //console.log(myName);
+    alert(" in A galaxy Far far Away ");
+  } else {
+    alert(
+      " Invalid Entry, Try Again ! "
+    );
+  }
+
+}
+
+let ex2 = true;
+ 
+
+
+
+
+
+//POE
+ex1();
